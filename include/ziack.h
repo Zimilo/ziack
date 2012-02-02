1e035dd35fe1ced309afd9ceb43f652b92a5ac09
Zimilo <zimilo@code-trick.com>
2012-02-02 20:58:59 +0800

    initial

---
+++ include/ziack.h
@@ -0,0 +1,15 @@
+/**
+ * zimilo@code-trick.com
+ */
+
+#ifndef __ZIACK_H__
+#define __ZIACK_H__
+
+#include "ziack_config.h"
+#include "ziack_version.h"
+#include "ziack_api.h"
+#include "ziack_types.h"
+
+
+
+#endif // !__ZIACK_H__
